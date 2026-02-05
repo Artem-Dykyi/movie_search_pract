3c5cd897d586b5621f0ca1d0cb6cb1e1a2f3d9f8
Артем <141495242+Artem-Dykyi@users.noreply.github.com>
2026-02-05 11:54:28 +0200

    deploy

--- filmsearch/src/index.jsx
+++ filmsearch/src/index.jsx
@@ -9,7 +9,7 @@ import { BrowserRouter } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/movie_search_pract">
       <App />
     </BrowserRouter>
   </React.StrictMode>
